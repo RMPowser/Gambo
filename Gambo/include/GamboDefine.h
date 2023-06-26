c09acdaa9f8b415c44c6bc3ee9d903bc0a2140d1
Ryan <rpryanpow@gmail.com>
2023-06-25 22:45:29 -0700

    interrupt flags are defined in binary for clarity

--- Gambo/include/GamboDefine.h
+++ Gambo/include/GamboDefine.h
@@ -177,11 +177,11 @@ enum ColorIndex
 
 enum InterruptFlags
 {
-	VBlank	= 1 << 0,
-	LCDStat	= 1 << 1,
-	Timer	= 1 << 2,
-	Serial	= 1 << 3,
-	Joypad	= 1 << 4,
+	VBlank	= 0b00000001,
+	LCDStat	= 0b00000010,
+	Timer	= 0b00000100,
+	Serial	= 0b00001000,
+	Joypad	= 0b00010000,
 };
 
 static SDL_Color GameBoyColors[5]
