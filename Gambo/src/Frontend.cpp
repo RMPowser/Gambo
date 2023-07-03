914cd046e970291eb7133cb1f4bcec4a8d6d4fdc
Ryan <rpryanpow@gmail.com>
2023-07-03 14:26:18 -0700

    renamed basic build configurations and added imgui files for Win32 and DirectX 12

--- Gambo/src/Frontend.cpp
+++ Gambo/src/Frontend.cpp
@@ -2,17 +2,26 @@
 
 #include "imgui.h"
 
-#ifdef _DEBUG
-#define DX12_ENABLE_DEBUG_LAYER
-#endif
+#ifdef WIN32_DX12
+	#include "imgui_impl_win32.h"
+	#include "imgui_impl_dx12.h"
+	#include <d3d12.h>
+	#include <dxgi1_4.h>
 
-#ifdef DX12_ENABLE_DEBUG_LAYER
-#include <dxgidebug.h>
-#pragma comment(lib, "dxguid.lib")
+	#ifdef _DEBUG
+	#define DX12_ENABLE_DEBUG_LAYER
+	#endif
+
+	#ifdef DX12_ENABLE_DEBUG_LAYER
+	#include <dxgidebug.h>
+	#pragma comment(lib, "dxguid.lib")
+	#endif
 #endif
 
 Frontend::Frontend()
 {
+	// create window
+	WNDCLASSEXW wc;
 }
 
 Frontend::~Frontend()
