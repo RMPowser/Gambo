01815fbaf17e2fd6940f49cd263704952c0a3e82
Ryan <rpryanpow@gmail.com>
2023-07-22 00:29:01 -0700

    "fixed" vram corruption from ignoring writes if ppu is in oamscan or draw modes

--- Gambo/src/CPU.cpp
+++ Gambo/src/CPU.cpp
@@ -69,14 +69,15 @@ u8 CPU::Read(u16 addr)
 
 void CPU::Write(u16 addr, u8 data)
 {
-	if ((core->ppu->IsEnabled()) && 
-		(
-			(core->ppu->GetMode() == PPUMode::OAMScan && (0xFE00 <= addr && addr <= 0xFE9F)) ||										// accessing oam during oam scan
-			(core->ppu->GetMode() == PPUMode::Draw && ((0xFE00 <= addr && addr <= 0xFE9F) || (0x8000 <= addr && addr <= 0x9FFF)))	// accessing oam or vram during drawing
-		)) 
-	{
-		return;
-	}
+	// TODO: fix this. it causes vram corruption because timing are wrong somewhere.
+	//if ((core->ppu->IsEnabled()) && 
+	//	(
+	//		(core->ppu->GetMode() == PPUMode::OAMScan && (0xFE00 <= addr && addr <= 0xFE9F)) ||										// accessing oam during oam scan
+	//		(core->ppu->GetMode() == PPUMode::Draw && ((0xFE00 <= addr && addr <= 0xFE9F) || (0x8000 <= addr && addr <= 0x9FFF)))	// accessing oam or vram during drawing
+	//	)) 
+	//{
+	//	return;
+	//}
 
     core->Write(addr, data);
 	if (addr == HWAddr::DMA)
