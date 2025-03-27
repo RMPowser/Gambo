cf1d4e9b56b82fe9bdd050c166627f9c82958f17
Ryan Powser <rpryanpow@gmail.com>
2025-03-26 23:34:28 -0700

    i dont think we actually need to copy the first 2 banks of the cartridge into ram on load anymore. that was only because we didnt have a cartridge class to handles reads directly from it.

--- Gambo/src/GamboCore.cpp
+++ Gambo/src/GamboCore.cpp
@@ -191,10 +191,6 @@ void GamboCore::InsertCartridge(std::filesystem::path filePath)
 	Reset();
 
 	cart->Load(filePath);
-
-	// copy the first 2 rom banks of the cartridge data into ram
-	for (u16 i = 0; i < 32_KB; i++)
-		ram->Set(i, cart->Read(i));
 }
 
 void GamboCore::SetUseBootRom(bool b)
